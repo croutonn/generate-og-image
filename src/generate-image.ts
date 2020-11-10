55449699a9141cbea0efceebd0c3e649e4608344
croutonn <hamintg@gmail.com>
2020-11-11 00:57:09 +0900

    Fix issue that webfonts will not display

--- src/generate-image.ts
+++ src/generate-image.ts
@@ -1,20 +1,21 @@
-import puppeteer from "puppeteer-core";
-import { IViewport } from "./types";
+import puppeteer from 'puppeteer-core'
+import { IViewport } from './types'
 
 async function generateImage(viewport: IViewport, html: string) {
   const browser = await puppeteer.launch({
-    executablePath: "/usr/bin/google-chrome-unstable",
-    args: ["--no-sandbox"]
-  });
-  const page = await browser.newPage();
+    executablePath: '/usr/bin/google-chrome-unstable',
+    args: ['--no-sandbox']
+  })
+  const page = await browser.newPage()
   page.setViewport({
     width: +viewport.width,
     height: +viewport.height
-  });
-  await page.setContent(html);
-  const image = await page.screenshot({ encoding: "base64" });
-  await browser.close();
-  return image;
+  })
+  await page.setContent(html)
+  await page.evaluateHandle('document.fonts.ready')
+  const image = await page.screenshot({ encoding: 'base64' })
+  await browser.close()
+  return image
 }
 
-export default generateImage;
+export default generateImage
